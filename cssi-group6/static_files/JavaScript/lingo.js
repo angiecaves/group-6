6728c228c2393a1630809f8ad33d1d20f7f603c8
phillipseb <phillemi77@gmail.com>
2016-08-02 17:08:38 -0500

    writing in screen

--- cssi-group6/static_files/JavaScript/lingo.js
+++ cssi-group6/static_files/JavaScript/lingo.js
@@ -12,21 +12,21 @@ var first_round
 
 function start_game(){
     random_word = get_random_word(words_list);
-    console.log(random_word)
+    document.write(random_word)
     first_in_word = random_word[0];
     first_round = [first_in_word, "_ ", "_ ", "_ ", "_ "];
-    console.log(first_round);
+    document.write(first_round);
 }
 
 function compare(random_w, guess){
 	if (random_w==guess){
-		console.log(guess)
+		document.write(guess)
 		for(i=0; i<5; i++){
 			first_round[i]=guess[i];
 		}
 		count=0;
-		console.log("You got it!");
-		console.log(first_round);
+		document.write("You got it!");
+		document.write(first_round);
 	}
 	else{
 		for (i=0; i<5; i++)
@@ -39,7 +39,7 @@ function compare(random_w, guess){
 			{
 				first_round[i]="("+guess+")";
 			}
-		console.log(first_round);
+		document.write(first_round);
 		}
 	}
 }
@@ -50,7 +50,7 @@ function redirect_to_winner(){
 
 function doGuess(){
    var user_guess = $("#user").val();
-   console.log(user_guess)
+   document.write(user_guess)
    compare(random_word,user_guess);
    count = count--;
    redirect_to_winner();
