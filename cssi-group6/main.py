feb277479d22d133ac384338e12d10c1fcb28a9c
phillipseb <phillemi77@gmail.com>
2016-08-02 14:40:18 -0500

    STEPHO STINKS

--- cssi-group6/main.py
+++ cssi-group6/main.py
@@ -38,7 +38,7 @@ first_round = [first_in_word, "_ ", "_ ", "_ ", "_ "]
 logging.info(first_round) # was print
 
 
-def compare(random_w,guess):
+def compare(MainHandler, random_w,guess):
     if random_w == guess: #if right on first try
         for i in range(0,5):
             first_round[i] = guess[i]
@@ -47,6 +47,7 @@ def compare(random_w,guess):
         logging.info(first_round) # was print
         # random_word = get_random_word(words_list) # dont trust this line
         #exit()
+        MainHandler.redirect("/winner")
     else: #if not right on first try
         for i in range(0,5):
             if random_w[i] == guess[i]:
@@ -61,7 +62,7 @@ class MainHandler(webapp2.RequestHandler):
         while (count > 0):
             user_guess = self.request.get("guess", "     ") # default value so it doesnt freak
             if (len(user_guess) == 5):
-                compare(random_word,user_guess)
+                compare(self,random_word,user_guess)
                 count = count -1
             else:
                 self.response.write("Please put in a five letter word")
--- cssi-group6/static_files/JavaScript/lingo.js
+++ cssi-group6/static_files/JavaScript/lingo.js
@@ -1,3 +1,9 @@
 function refresh_page(){
 	window.location.reload()
 }
+
+function redirect_to_winner(){
+	window.location.replace("/winner")
+}
+
+$(document).ready(setup)
