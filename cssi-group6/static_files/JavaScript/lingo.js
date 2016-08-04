6830d75807a805b11618e11b0642e63725f0cdbd
phillipseb <phillemi77@gmail.com>
2016-08-04 14:05:13 -0500

    HELP

--- cssi-group6/static_files/JavaScript/lingo.js
+++ cssi-group6/static_files/JavaScript/lingo.js
@@ -1,7 +1,7 @@
 function get_random_word(words){
     var randomIndex = Math.floor(Math.random() * words.length); 
-	var randomElement = words[randomIndex];
-	return randomElement;
+    var randomElement = words[randomIndex];
+    return randomElement;
 }
 
 var words_list = ['bingo','bacon','tears','dream','click','stick','right','taken','group','chair','water','store','brain',"youth",'about','apple','mouth','house','check','break','barns','start','chase','train','choir','clash','cough','grape','melon','fruit','steak','fries','chips','badge','watch','trunk','tiger','stage','happy'];
@@ -14,8 +14,8 @@ var score = 0;
 
 
 function start_game(){
-	count = 5;
-	$("#boxed").html(count);
+    count = 5;
+    $("#boxed").html(count);
     random_word = get_random_word(words_list);
     console.log(random_word);
     //document.write(random_word)
@@ -26,43 +26,42 @@ function start_game(){
 
 function redirect_to_winner(){
     $("#playWin").html("Correct word: " + first_round);
-	window.location.replace("/winner");
+    window.location.replace("/winner");
 }
 
 function double_letter(str, letter){
-		x=str.replace(/[^letter]/g, "").length;
-		return x;
+        x=str.replace(/[^letter]/g, "").length;
+        return x;
 }
 
 function compare(random_w, guess){
-
-	if (random_w==guess){
-		for(i=0; i<5; i++){
-			first_round[i]="[" + guess[i] + "]";
-		}
-		redirect_to_winner();
-	}
-	else{
-		for (i=0; i<5; i++)
-		{
-			if (random_w[i]==guess[i])
-			{
-				first_round[i]="[" + guess[i] + "]";
-			}
-		    else if (random_w.includes(guess[i]) == true && first_round.toString().indexOf("[") == -1)
-			{
-				var in_random=random_w.replace(/[^random_w[i]]/g, "").length;
-				var in_guess = guess.replace(/[^random_w[i]]/g, "").length;
-				if(in_random>=in_guess)
-				{
-					first_round[i]="("+guess[i]+")";
-				}
-			}
-		$("#output").html(first_round);
-		}
-		count = count -1;
-		$("#boxed").html(count);
-	}
+    if (random_w==guess){
+        for(i=0; i<5; i++){
+            first_round[i]="[" + guess[i] + "]";
+        }
+        redirect_to_winner();
+    }
+    else{
+        for (i=0; i<5; i++)
+        {
+            if (random_w[i]==guess[i])
+            {
+                first_round[i]="[" + guess[i] + "]";
+            }
+            else if (random_w.includes(guess[i]) == true && first_round.toString().indexOf("[" + guess[i] + "]") == -1)
+            {
+                var in_random=random_w.replace(/[^random_w[i]]/g, "").length;
+                var in_guess = guess.replace(/[^random_w[i]]/g, "").length;
+                if (in_random >= in_guess)
+                {
+                    first_round[i]="("+guess[i]+")";
+                }
+            }
+        $("#output").html(first_round);
+        }
+        count = count -1;
+        $("#boxed").html(count);
+    }
 }
 
 
@@ -71,25 +70,25 @@ function doGuess(){
    var user_guess = $("#user").val();
    if (user_guess.length != 5)
    {
-   	 alert("Please enter a five letter word!!")
+     alert("Please enter a five letter word!!")
    }
    else
    {
-   $("#output").html(user_guess)
-   compare(random_word,user_guess);
+      $("#output").html(user_guess)
+      compare(random_word,user_guess);
    }
 
    if (count < 1)
    {
-   	 alert("YOU LOSE");
-   	 window.location.reload();
+     alert("YOU LOSE");
+     window.location.reload();
    }
 }
 
 function setup(){
     start_game();
     // var count = 10
-    $("#myButton").click(doGuess); 	
+    $("#myButton").click(doGuess);  
 }
 
 $(document).ready(setup)
